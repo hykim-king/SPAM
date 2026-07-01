fc20d7228230d459ac6cfe8ddcf9c68feb53c25e
tnals <tnals5035@gmail.com>
2026-07-01 15:32:52 +0900

    account ÆÐŰÁö µî·Ï

---
+++ src/main/java/com/pcwk/ehr/account/mapper/AccountMapper.java
@@ -0,0 +1,21 @@
+package com.pcwk.ehr.account.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.pcwk.ehr.account.domain.AccountVO;
+import com.pcwk.ehr.cmn.DTO;
+import com.pcwk.ehr.cmn.WorkDiv;
+
+@Mapper
+public interface AccountMapper extends WorkDiv<AccountVO> {
+
+	
+	List<AccountVO> doRetrieveANSI (DTO param);
+	
+	int saveAll(int saveCount);
+	List<AccountVO> getAllMember();
+	int totalCnt();
+	int deleteAll();
+}
